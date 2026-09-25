96b88039bb8b6682be694efc14642f3b66e4f649
norafidamn-cloud <norafidamn@gmail.com>
2026-09-25 13:05:17 +0800

    Add files via upload

--- profile.js
+++ profile.js
@@ -200,26 +200,20 @@ document.addEventListener("DOMContentLoaded", function () {
        NAVIGATION ACTIVE STATE
     ========================================= */
 
-    const navItems =
-        document.querySelectorAll(".nav-item");
+const navItems = document.querySelectorAll(".nav-item");
 
+navItems.forEach(function (item) {
 
-    navItems.forEach(function (item) {
+    item.addEventListener("click", function () {
 
-        item.addEventListener("click", function (e) {
-
-            e.preventDefault();
-
-            navItems.forEach(function (nav) {
-
-                nav.classList.remove("active");
-
-            });
-
-            item.classList.add("active");
-
+        navItems.forEach(function (nav) {
+            nav.classList.remove("active");
         });
 
+        item.classList.add("active");
+
     });
+
+});
 
 });
